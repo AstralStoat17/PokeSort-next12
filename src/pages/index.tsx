3a775b55f07a5c63305b0315d489a18d602251b4
AstralStoat17 <232106207+AstralStoat17@users.noreply.github.com>
2026-03-01 20:37:12 +0100

    Add decent enough loading state

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,7 +3,9 @@ import { trpc } from "@/utils/trpc";
 import { useState } from "react";
 import type React from "react";
 import { inferQueryResponse } from "./api/trpc/[trpc]";
+
 import Image from "next/image";
+import Link from "next/link";
 
 const btn =
   "inline-flex items-center px-2.5 py-1.5 border border-gray-300 shadow-sm text-xs font-medium rounded text-gray-700 bg-white hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500";
@@ -28,31 +30,36 @@ export default function Home() {
     updateIds(getOptionsForVote());
   };
 
+  const dataLoaded =
+    !firstPokemon.isLoading &&
+    firstPokemon.data &&
+    !secondPokemon.isLoading &&
+    secondPokemon.data;
+
   return (
-    <div className="h-screen w-screen flex flex-col justify-center items-center relative">
-      <div className="text-2xl text-center">Which Pokémon is Rounder?</div>
-      <div className="p-2" />
-      <div className="border rounded p-8 flex justify-between items-center max-w-2xl">
-        {!firstPokemon.isLoading &&
-          firstPokemon.data &&
-          !secondPokemon.isLoading &&
-          secondPokemon.data && (
-            <>
-              <PokemonListing
-                pokemon={firstPokemon.data}
-                vote={() => voteForRoundest(first)}
-              />
-              <div className="p-8">Vs</div>
-              <PokemonListing
-                pokemon={secondPokemon.data}
-                vote={() => voteForRoundest(second)}
-              />
-            </>
-          )}
-        <div className="p-2" />
-      </div>
-      <div className="absolute bottom-0 w-full text-xl text-center pb-2">
-        <a href="https://github.com/AstralStoat17/PokeSort">Github</a>
+    <div className="h-screen w-screen flex flex-col justify-between items-center relative">
+      <div className="text-2xl text-center pt-8">Which Pokémon is Rounder?</div>
+      {dataLoaded && (
+        <div className="border rounded p-8 flex justify-between items-center max-w-2xl">
+          <PokemonListing
+            pokemon={firstPokemon.data}
+            vote={() => voteForRoundest(first)}
+          />
+          <div className="p-8">Vs</div>
+          <PokemonListing
+            pokemon={secondPokemon.data}
+            vote={() => voteForRoundest(second)}
+          />
+          <div className="p-2" />
+        </div>
+      )}
+      {!dataLoaded && <img src="/rings.svg" className="w-48" />}
+      <div className="w-full text-xl text-center pb-2">
+        <a href="https://github.com/TheoBr/roundest-mon">Github</a>
+        {" | "}
+        <Link href="/results">
+          <a>Results</a>
+        </Link>
       </div>
     </div>
   );
@@ -72,7 +79,7 @@ const PokemonListing: React.FC<{
         height={256}
         layout="fixed"
       />
-      <div className="text-xl text-center capitalize mt-[0.5rem]">
+      <div className="text-xl text-center capitalize mt-[-0.5rem]">
         {props.pokemon.name}
       </div>
       <button className={btn} onClick={() => props.vote()}>
